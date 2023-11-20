c4e031e7b47fcad0f60c227fc8119e88371ca8b3
pablocrosio <pablo_crosio@yahoo.com.ar>
2023-11-19 22:29:36 -0300

    Primer Commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/pablocrosio/godesde0
+
+go 1.21.4
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pablocrosio/godesde0/variables"
+)
+
+func main() {
+	variables.MuestroEnteros()
+}
